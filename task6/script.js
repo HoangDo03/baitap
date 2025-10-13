97f8965aa86ad48605cef3148db65cfd534e19b6
Xuan Hoang <hoangdnx03@gmail.com>
2025-10-13 16:05:06 +0700

    task6

--- task6/script.js
+++ task6/script.js
@@ -313,8 +313,9 @@ function drawTextOnArcs() {
     svg.appendChild(path);
 
     const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    text.setAttribute("class","text-svg-chart")
     text.setAttribute("fill", "white");
-    text.setAttribute("font-size", "25");
+    text.setAttribute("font-size", "23");
     text.setAttribute("font-weight", "700");
     text.setAttribute("text-anchor", "middle");
 
